283a68d2efcbd946e6718b6c16c9165ce92a7b5c
Marcial Lalanda <mlalandag@gmail.com>
2021-08-07 20:20:51 +0200

    refactorización

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
 # from containers import  Configs, Agents, Environments
-from environments.environment import K8Senvironment
-from agents.DQNAgent          import DQNAgent
-from config                   import NUM_EPISODES
+from environments.environment  import K8Senvironment
+from agents.DQNAgent           import DQNAgent
+from configuration             import configuration
+from environments.pods_cpu_mem import K8Sstate
 
 import time
 import random
 
 if __name__ == "__main__":
 
+    batch_size = 100
     # Creamos el entorno y el agente       
     env = K8Senvironment()
     agent = DQNAgent(env.action_space)
     total_reward = 0
-
-    for i in range(NUM_EPISODES):        
-        # Inicializamos state, reward y done
-        reward  = 0
-        done    = False            
-        past_num_tasks = 0
+    state = env.get_state()
     
+    for i in range(configuration.NUM_EPISODES):        
+        # Inicializamos reward
+        reward  = 0
+        past_num_tasks = 0
 
-        while not done:
+        for t in range(configuration.NUM_TIMESTEPS):
 
             # El agente decide la siguiente accion y recoge 
             # el nuevo estado y la recompensa
-            action = agent.act(state, reward, done)
-            next_state, reward, done, info = env.step(action)
+            action = agent.act(state)
+            next_state, reward = env.step(action)
+
+            #store the transition information
+            agent.store_transition(state, action, reward, next_state)
+
+            state = next_state
             # agent.update_network parameters
             total_reward += reward
-            state = next_state     
-            print("state = {}, reward= {}, done = {}, total_reward={:.2f}".format(state, reward, done, total_reward))              
+
+            print("state = {}, reward= {}, total_reward={:.2f}".format(state, reward, total_reward))      
+
+            if len(agent.replay_buffer) > batch_size:
+                agent.train(batch_size)        
+
+            time.sleep(3)                
