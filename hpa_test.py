c850ab63542c01b3dca1add57c634fc1a37f54d4
Marcial Lalanda <mlalandag@gmail.com>
2021-09-18 10:48:37 +0200

    First Main test with 2-4 model

--- hpa_test.py
+++ hpa_test.py
@@ -16,27 +16,25 @@ if __name__ == "__main__":
 
         count += 1  
    
-        time.sleep(30)
+        time.sleep(15)
         state = env.get_state()
         
         #number_of_pods = int(state[0][0])
         number_of_pods = sum(1 for i in state[0] if i != 0)
         cpu_usage = state[0][0:configuration.MAX_NUM_PODS]
-        print("cpu_usage = {}".format(cpu_usage))
         total_cpu_usage = np.sum(cpu_usage)
-        print("total_cpu_usage = {}".format(total_cpu_usage))
         buffer_number_of_pods.append(number_of_pods)
         buffer_total_cpu_usage.append(total_cpu_usage)     
         buffer_datetime.append(datetime.datetime.now())
 
-        if count % 10 == 0:
+        if count % 4 == 0:
             print("Plotting pods vs cpu")
             graph = plt.figure()
             plt.ylabel('number of pods')
             plt.xlabel('cpu usage')
             plt.yticks(range(1, configuration.MAX_NUM_PODS))
             plt.plot(buffer_total_cpu_usage, buffer_number_of_pods, 'o', color='black')
-            plotfile = "./graphs/hpa/performance_train.jpg"
+            plotfile = "./graphs/hpa/performance_hpa.jpg"
             graph.savefig(plotfile)  
 
             print("Plotting pods vs time")
@@ -46,7 +44,7 @@ if __name__ == "__main__":
             plt.yticks(range(1, configuration.MAX_NUM_PODS))
             plt.plot(buffer_datetime, buffer_number_of_pods, 'bo-')
             plt.gcf().autofmt_xdate()
-            plotfile = "./graphs/hpa/performance_train_pods_time.jpg"
+            plotfile = "./graphs/hpa/main_performance_hpa_pods_time.jpg"
             graph.savefig(plotfile)    
 
             print("Plotting cpu vs time")
@@ -56,6 +54,6 @@ if __name__ == "__main__":
             plt.yticks(range(1, configuration.MAX_NUM_PODS))
             plt.plot(buffer_datetime, buffer_total_cpu_usage)
             plt.gcf().autofmt_xdate()
-            plotfile = "./graphs/hpa/performance_train_cpu_time.jpg"
+            plotfile = "./graphs/hpa/performance_hpa_cpu_time.jpg"
             graph.savefig(plotfile)   
                 
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from configuration              import configuration
 from collections                import deque
 import matplotlib.pyplot        as plt
 import numpy                    as np
-
+import datetime
 import time
 import random
 
@@ -21,7 +21,8 @@ if __name__ == "__main__":
     reward  = 0
     state = env.get_state()
     buffer_number_of_pods  = []
-    buffer_total_cpu_usage = []    
+    buffer_total_cpu_usage = []
+    buffer_datetime = []  
     count = 0
 
     while True:  
@@ -30,27 +31,46 @@ if __name__ == "__main__":
         print("count = {}".format(count))
 
         action = agent.act(state, False)
-        next_state, reward = env.step(action)
+        next_state, reward = env.step(state, action)
         total_reward += reward        
         print("state = {}, action = {}, reward= {}, total_reward={:.2f}".format(state, action, reward, total_reward))       
-        time.sleep(30)
-        state = env.get_state()
+        time.sleep(15)
+        state = next_state
         
         #number_of_pods = int(state[0][0])
         number_of_pods = sum(1 for i in state[0] if i != 0)
         cpu_usage = state[0][0:configuration.MAX_NUM_PODS]
-        print("cpu_usage = {}".format(cpu_usage))
         total_cpu_usage = np.sum(cpu_usage)
-        print("total_cpu_usage = {}".format(total_cpu_usage))
         buffer_number_of_pods.append(number_of_pods)
-        buffer_total_cpu_usage.append(total_cpu_usage)     
+        buffer_total_cpu_usage.append(total_cpu_usage)    
+        buffer_datetime.append(datetime.datetime.now()) 
 
-        if count % 3 == 0:
-            print("Plotting")
+        if count % 4 == 0:
+            print("Plotting pods vs cpu")
             graph = plt.figure()
             plt.ylabel('number of pods')
             plt.xlabel('cpu usage')
             plt.yticks(range(1, configuration.MAX_NUM_PODS))
             plt.plot(buffer_total_cpu_usage,buffer_number_of_pods, 'o', color='black')
-            graph.savefig('./graphs/dlr/performance.jpg')   
+            graph.savefig('./graphs/drl/main_performance.jpg') 
+
+            print("Plotting pods vs time")
+            graph = plt.figure()
+            plt.ylabel('number of pods')
+            plt.xlabel('time')
+            plt.yticks(range(1, configuration.MAX_NUM_PODS))
+            plt.plot(buffer_datetime, buffer_number_of_pods)
+            plt.gcf().autofmt_xdate()
+            plotfile = "./graphs/drl/main_performance_pods_time.jpg"
+            graph.savefig(plotfile)    
+
+            print("Plotting cpu vs time")
+            graph = plt.figure()
+            plt.ylabel('cpu')
+            plt.xlabel('time')
+            plt.yticks(range(1, configuration.MAX_NUM_PODS))
+            plt.plot(buffer_datetime, buffer_total_cpu_usage)
+            plt.gcf().autofmt_xdate()
+            plotfile = "./graphs/drl/main_performance_cpu_time.jpg"
+            graph.savefig(plotfile)       
                 
